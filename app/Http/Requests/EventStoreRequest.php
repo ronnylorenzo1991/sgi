811693d0a208717a17c5469e8a9a153ef0314d8c
Damian Gazmuriz Adan <dgazmuriz@gmail.com>
2022-11-08 11:10:30 -0500

    add formrequest Category and Subcategory

---
+++ app/Http/Requests/EventStoreRequest.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EventStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'date' => 'required|date',
+            'number' => 'required|unique:events,number',
+            'category_id' => 'required',
+            'observations' => 'required',
+            'detected_by_id' => 'required',
+            'subcategory_id' => 'required',
+            'contribute_id' => 'required',
+            'internet_link_id' => 'required',
+            'country_id' => 'required',
+            'ministry_id' => 'required',
+            'ips' => 'required',
+        ];
+        return $rules;
+    }
+
+    public function attributes()
+    {
+        return [
+            'date'=> 'Fecha',
+            'number'=> 'Número',
+            'category_id'=> 'Categoría',
+            'observations'=> 'Observaciones',
+            'detected_by_id'=> 'Detectado Por',
+            'subcategory_id'=> 'Subcategoría',
+            'contribute_id'=> 'Tributa',
+            'internet_link_id'=> 'Enlace',
+            'country_id'=> 'Entidad',
+            'ministry_id'=> 'Ministerio',
+            'ips'=> 'UM secundario 2',
+        ];
+    }
+}
---
+++ app/Http/Requests/SubCategoryUpdateRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SubCategoryUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $rules = [
+            'name' => 'required|unique:subcategories,name,'.request()->id,
+            'description' => 'required',
+        ];
+        return $rules;
+    }
+
+    public function attributes()
+    {
+        return [
+            'name'=> 'Nombre',
+            'description'=> 'Descripción',
+        ];
+    }
+}
